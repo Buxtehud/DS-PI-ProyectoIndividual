cdd7fce20b0ad8f119776d5a7f8bf699a3fbba5a
Buxtehud <andres.anreyes@gmail.com>
2022-06-22 01:16:45 -0300

    Limpieza de datos

---
+++ pipeline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+df_clientes = pd.read_csv('./Datasets/Clientes.csv', delimiter=';', encoding='utf-8')
+df_compra = pd.read_csv('./Datasets/Compra.csv')
+df_gasto = pd.read_csv('./Datasets/Gasto.csv')
+df_local = pd.read_csv('./Datasets/Localidades.csv')
+df_prov = pd.read_csv('./Datasets/Proveedores.csv', encoding='iso-8859-1')
+df_suc = pd.read_csv('./Datasets/Sucursales.csv', delimiter=';')
+df_venta = pd.read_csv('./Datasets/Venta.csv')
+
+# ------------Para tabla ventas----------------------------------
+
+
+def get_moda(df, element_id, referencia, target):
+    """Obtiene la moda de una columna target filtrando a través de la columna referencia"""
+    df_moda = df[df[referencia] == element_id].mode()
+    return df_moda[target][0]
+
+
+def input_moda(df, element_id, referencia, target):
+    """Cambia todos los valores de las columnas al valor de la moda"""
+    moda = get_moda(df, element_id, referencia, target)
+    df.loc[(df[target] != moda) & (df[referencia] == element_id), target] = moda
+
+
+productos = [x for x in df_venta['IdProducto'].unique()] # Obtengo todos los Id únicos de cada producto
+
+
+for product in productos:
+    input_moda(df_venta, product, 'IdProducto', 'Precio')
